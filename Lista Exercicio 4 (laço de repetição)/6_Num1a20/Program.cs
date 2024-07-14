4cc0b1676f56792492abfb6cde7d0374a2357ddb
coderBiell <resendegabriel137@gmail.com>
2024-07-14 18:24:12 -0300

    atv 6 7 8

--- Lista Exercicio 4 (laço de repetição)/6_Num1a20/Program.cs
+++ Lista Exercicio 4 (laço de repetição)/6_Num1a20/Program.cs
@@ -1,1 +1,27 @@
-﻿// 6
+﻿// 6  Faça um programa que imprima na tela os números de 1 a 20, um abaixo do outro. Depois modifique o programa para que ele mostre os números um ao lado do outro. 
+using System;
+namespace Num1a20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 20; i++)
+            {
+                Console.WriteLine($"{i}");
+
+            }
+            for (int i = 1; i <= 20; i++)
+            {
+                Console.Write($"{i} ");
+
+            }
+
+
+
+
+
+
+        }
+    }
+}
---
+++ Lista Exercicio 4 (laço de repetição)/8_NumSomaMedia/Program.cs
@@ -0,0 +1,21 @@
+﻿//8- Faça um programa que leia 5 números e informe a soma e a média dos números
+using System;
+namespace SomaMedia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double soma = 0;
+
+            for (int i = 0;i < 5; i++){
+                Console.Write("Digite um numero: ");
+                double num = Convert.ToDouble(Console.ReadLine());
+
+                soma += num;
+            }
+
+            Console.WriteLine($"A soma dos numeros e de: {soma} \nA media os numeros e: {soma / 5}");
+        }
+    }
+}
