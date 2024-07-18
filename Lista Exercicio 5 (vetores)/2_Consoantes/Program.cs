1df15a78c660dd2e5d475e1e2138603ec518d3dc
GabrielResendel <codegabriel7@gmail.com>
2024-07-18 19:21:26 -0300

    atv 2 3

---
+++ Lista Exercicio 5 (vetores)/2_Consoantes/Program.cs
@@ -0,0 +1,35 @@
+﻿//  2 Faça um Programa que leia um vetor de 10 caracteres, e diga quantas consoantes foram lidas. Imprima as consoantes (Entenda que serão digitados somente letras do alfabeto sem acento).
+using System;
+namespace consoantes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] Letras = new char[10];
+            int cont = 0;
+            
+
+                Console.WriteLine("Digite 10 letras do alfabeto: ");
+                for (int i = 0; i < 10; i++){
+                    Letras[i] = char.Parse(Console.ReadLine());
+                }
+
+
+            Console.WriteLine($"\nForam Digitadas essas consoantes: ");
+
+                for (int i = 0; i < 10; i++){
+
+                    if(Letras[i] == 'a' || Letras[i] =='e'|| Letras[i] =='i'|| Letras[i] =='o'|| Letras[i] =='u'){
+                        cont++;
+                        Console.Write($"{Letras[i]} ");
+                    }
+
+                }
+                
+            Console.Write($"\nNo total foram {cont} consoantes digitadas!!");
+
+
+        }
+    }
+}
---
+++ Lista Exercicio 5 (vetores)/3_VetorImparPar/Program.cs
@@ -0,0 +1,66 @@
+﻿// 3 Faça um Programa que leia 20 números inteiros e armazene-os num vetor. Armazene os números pares no vetor PAR e os números IMPARES no vetor impar. Imprima os três vetores.
+using System;
+namespace VetorImparPar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Vetor = new int[20];
+            int contImpar = 0;
+            int contPar = 0;
+
+            Console.WriteLine("Digite 20 numeros inteiros: ");
+
+            for (int i = 0; i < 20; i++){
+                Vetor[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (int i = 0;i < 20; i++){
+
+                if(Vetor[i] % 2 == 0){
+                    contPar++;
+                }else {
+                    contImpar++;
+                }
+            }
+
+            int[] Impar = new int[contImpar];
+            int[] Par = new int[contPar];
+            int IndexPar = 0;
+            int IndexImpar = 0;
+
+            for (int i = 0;i < 20; i++){
+
+                if(Vetor[i] % 2 == 0){
+
+                    Par[IndexPar] = Vetor[i];
+                    IndexPar++;
+                }else {
+
+                    Impar[IndexImpar] = Vetor[i];
+                    IndexImpar++;
+                }
+            }
+            
+            Console.Write ("\nOs valores digitados foram:");
+            for(int i = 0;i < Vetor.Length; i++){
+                Console.Write($"{Vetor[i]} ");
+            }
+
+            Console.WriteLine("\nOs numeros pares foram:");
+            for(int i = 0;i < Par.Length; i++){
+                Console.Write($"{Par[i]} ");
+            }
+
+            Console.WriteLine("\nOs numeros Impares foram: ");
+            for(int i = 0;i < Impar.Length; i++){
+                Console.Write($"{Impar[i]} ");
+            }
+        
+
+
+
+        }
+    }
+}
