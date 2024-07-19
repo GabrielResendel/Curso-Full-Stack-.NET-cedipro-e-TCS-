70e2a3c9072a1b169cb2daca92439ee447eb247b
GabrielResendel <codegabriel7@gmail.com>
2024-07-19 19:56:43 -0300

    atv 4 5

---
+++ Lista Exercicio 5 (vetores)/4_SomaQuadrado/Program.cs
@@ -0,0 +1,25 @@
+﻿//4. Faça um Programa que leia um vetor A com 10 números inteiros, calcule e mostre a soma dos quadrados dos elementos do vetor.
+using System;
+namespace SomaQuadrado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] VetorA = new int[10];
+            int soma = 0;
+
+            Console.WriteLine("Digite 10 numeros inteiros e descubra sua soma ao quadrado: ");
+            for (int i = 0;i < 10; i++){
+                VetorA[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (int i = 0;i < 10; i++){
+
+                soma = VetorA[i] * VetorA[i];
+                Console.WriteLine($"Soma do elemento {i+1} e: {soma}");
+            }
+        }
+    }
+}
---
+++ Lista Exercicio 5 (vetores)/5_IdadeAltura/Program.cs
@@ -0,0 +1,41 @@
+﻿// 5. Foram anotadas as idades e alturas de 30 alunos. Faça um Programa que determine quantos alunos com mais de 13 anos possuem altura inferior à média de altura desses alunos.
+using System;
+namespace SomaQuadrado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        int[] Idade = new int[30];
+        double[] Altura = new double[30];
+
+            for (int i = 0;i < 30; i++){
+                Console.Write($"Digite a idade do aluno {i+1}: \n");
+                Idade[i] = int.Parse(Console.ReadLine());
+
+                Console.Write($"Agora a altura do aluno {i+1}: \n");
+                Altura[i] = Convert.ToDouble(Console.ReadLine());
+            }
+
+        double MediaAltura = 0;
+
+            for (int i = 0;i < 30; i++){
+                MediaAltura += Altura[i];
+            }
+
+        int CountAluno= 0;
+
+           for (int i = 0;i < 30; i++){
+            if (Idade[i] > 13 && Altura[i] < MediaAltura / 30){
+                CountAluno++;
+            }
+           }
+
+        Console.WriteLine($"O total de alunos com idade superior a 13 anos e altura inferior a media dos alunos que e de {MediaAltura / 30} são de {CountAluno} alunos. ");
+        }
+
+        
+ 
+    }    
+}
